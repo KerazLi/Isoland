b56a88e19200b26a317c34cc5a2b721e6af32b8a
Keraz <1530217461@qq.com>
2024-06-26 05:39:05 +0800

    场景切换的渐入渐出

--- Assets/Scripts/Transition/TransitionManager.cs
+++ Assets/Scripts/Transition/TransitionManager.cs
@@ -5,16 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class TransitionManager : Singleton<TransitionManager>
 {
+    public CanvasGroup fadeCanvasGroup;
+    public float fadeDuration;
+    public bool isFade;
     public void Transition(string from, string to)
     {
-        StartCoroutine(TransitionToScene(from, to));
+        if (!isFade)
+        {
+            StartCoroutine(TransitionToScene(from, to));
+        }
     }
 
     private IEnumerator TransitionToScene(string from,string goTo)
     {
+        yield return Fade(1);
         yield return SceneManager.UnloadSceneAsync(from);
         yield return  SceneManager.LoadSceneAsync(goTo,LoadSceneMode.Additive);
         Scene newScene = SceneManager.GetSceneAt(SceneManager.sceneCount - 1);
         SceneManager.SetActiveScene(newScene);
+        yield return Fade(0);
+    }
+
+    private IEnumerator Fade(float targetAlpha)
+    {
+        isFade = true;
+        fadeCanvasGroup.blocksRaycasts = true;
+        float speed = Mathf.Abs(fadeCanvasGroup.alpha - targetAlpha) / fadeDuration;
+        while (!Mathf.Approximately(fadeCanvasGroup.alpha,targetAlpha))
+        {
+            fadeCanvasGroup.alpha = Mathf.MoveTowards(fadeCanvasGroup.alpha, targetAlpha, speed * Time.deltaTime);
+            yield return null;
+        }
+        fadeCanvasGroup.blocksRaycasts = false;
+        isFade = false;
     }
 }
